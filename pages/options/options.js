471b09d075044686fd60fb857de9cd3ad550ef24
Bestony <xiqingongzi@gmail.com>
2017-02-07 17:40:25 +0800

    加入清除数据功能，方便调试

--- pages/options/options.js
+++ pages/options/options.js
@@ -1,19 +1,38 @@
 // pages/options/options.js
 Page({
-  data:{},
-  onLoad:function(options){
+  data: {},
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
+  },
+  cleanCache: function () {
+    wx.clearStorage({
+
+      success: function (res) {
+        wx.showToast({
+          title: '数据清除成功！',
+          icon: 'success',
+          duration: 3000
+        })
+
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
   }
 })
