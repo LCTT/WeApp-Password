6806ba1a5cd251c62909f7aa1a540ce8630f29a9
Bestony <xiqingongzi@gmail.com>
2017-02-08 20:59:55 +0800

    Fixed #21

--- pages/add/add.js
+++ pages/add/add.js
@@ -3,38 +3,54 @@ Page({
   data: {},
   onLoad: function (options) {
     var that = this;
+    if (wx.getStorageSync(options.secret) == '') {
+      
+    } else {
+        wx.showModal({
+           title: '该场景已经添加过！',
+          content: '当前场景已经添加过！点击取消继续修改，数据将被覆盖！',
+          success: function (res) {
+            if (res.confirm) {
+              wx.switchTab({
+                url: '../servers/servers'
+              })
+            }
+          }
+        })
+    }
     wx.getLocation({
-      type: 'wgs84',
-      success: function (res) {
-        that.setData({
-          name: decodeURI(options.name),
-          secret: options.secret,
-          username: decodeURI(options.username),
-          latitude: res.latitude,
-          longitude: res.longitude
-        });
-      },
-    })
+        type: 'wgs84',
+        success: function (res) {
+          that.setData({
+            name: decodeURI(options.name),
+            secret: options.secret,
+            username: decodeURI(options.username),
+            latitude: res.latitude,
+            longitude: res.longitude
+          });
+        },
+      })
+
 
   },
   submitData: function (e) {
-    var UUID = this.getUUID();
+    var that = this
     wx.setStorage({
-      key: UUID,
+      key: that.data.secret,
       data: {
-        secret: this.data.secret,
+        secret: that.data.secret,
         name: e.detail.value.name,
         username: e.detail.value.username,
         desc: e.detail.value.desc,
-        latitude: this.data.latitude,
-        longitude: this.data.longitude,
-        signedBy:this.data.name,
-        key:UUID
+        latitude: that.data.latitude,
+        longitude: that.data.longitude,
+        signedBy: that.data.name,
+        key: that.data.secret
       },
       success: function (res) {
-        
+
         wx.redirectTo({
-          url: '../addSucc/addSucc?id='+UUID
+          url: '../addSucc/addSucc?id=' + that.data.secret
         })
       },
       fail: function () {
@@ -52,10 +68,4 @@ Page({
       }
     })
   },
-  getUUID: function () {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  }
 })
