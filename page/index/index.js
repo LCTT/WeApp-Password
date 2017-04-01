1a3e546900a86f9c3c5e8365f42a5d37954fc2b9
Bestony <best.tony@foxmail.com>
2017-04-01 21:43:09 +0800

    改用 template 加载

--- page/index/index.js
+++ page/index/index.js
@@ -1,19 +1,60 @@
 // page/index/index.js
 Page({
-  data:{},
-  onLoad:function(options){
+  data: {
+    "server": [
+      {
+        username: "bestony@linux.cn",
+        name: "Linux 中国",
+        code: "123 456",
+        typed: "website",
+        key: "ABCSXS"
+      },
+      {
+        username: "bestony@linux.cn",
+        name: "Linux 中国",
+        code: "456 789",
+        typed: "website",
+        key: "ABCSXS"
+      }
+    ]
+  },
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
+  },
+  // 将生成的密钥放到剪贴板中，并提示。
+  copyCode: function (e) {
+    wx.setClipboardData({
+      data: e.currentTarget.dataset.code,
+      success: function (res) {
+        wx.showToast({
+          title: '代码已经复制到您的剪贴板中',
+          icon: 'success',
+          duration: 2000
+        })
+      }
+    })
+  },
+  viewInfo: function (e) {
+    wx.navigateTo({
+      url: '../view/view?id=' + e.currentTarget.dataset.key,
+      success: function (res) {
+        // success
+      }
+    })
+  },
+  addServer: function (e) {
+
   }
 })
