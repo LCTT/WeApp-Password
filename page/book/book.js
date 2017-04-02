039071ee631d9d380bb428de05f3a3db27c23e44
Bestony <best.tony@foxmail.com>
2017-04-02 11:38:03 +0800

    加入book页面

---
+++ page/book/book.js
@@ -0,0 +1,32 @@
+// page/book/book.js
+Page({
+  data:{
+    book:[
+      {
+        title:"在 Linux 服务器使用运维密码",
+        color:"mainRed"
+      },{
+        title:"在 Github 上使用运维密码",
+        color:"mainBlue"
+      },{
+        title:"在 Linux 中国上使用运维密码",
+        color:"mainYellow"
+      }
+    ]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
