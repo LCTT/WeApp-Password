47c83c35dca68bd6fa94dd1d839e64ce87cf8178
Bestony <best.tony@foxmail.com>
2017-04-02 08:53:36 +0800

    options 页面使用 导航器来实现

--- page/options/options.js
+++ page/options/options.js
@@ -1,6 +1,37 @@
 // page/options/options.js
 Page({
-  data:{},
+  data:{
+    config:[
+      {
+        "url":"/page/view/view",
+        "name":"数据备份",
+        "desc":"备份/恢复",
+        "color":"mainRed",
+        "type" :"website"
+      },
+      {
+        "url":"/page/view/view",
+        "name":"云服务",
+        "desc":"同步云端",
+        "color":"mainBlue",
+         "type" :"website"
+      },
+      {
+        "url":"/page/view/view",
+        "name":"使用指南",
+        "desc":"教你玩转运维密码",
+        "color":"mainGreen",
+         "type" :"website"
+      },
+      {
+        "url":"/page/view/view",
+        "name":"关于我们",
+        "desc":"运维密码和我们",
+        "color":"mainYellow",
+         "type" :"website"
+      }
+    ]
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
