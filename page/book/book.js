85265a1232b0c5437567a409921550ff6ef0c538
Bestony <best.tony@foxmail.com>
2017-04-03 09:19:08 +0800

    加入链接

--- page/book/book.js
+++ page/book/book.js
@@ -1,32 +1,38 @@
 // page/book/book.js
 Page({
-  data:{
-    book:[
+  data: {
+    book: [
+      
+      // {
+      //   title: "在 Linux 服务器使用运维密码",
+      //   color: "mainRed",
+      //   url: "/page/book/linux"
+      // }, 
+      
       {
-        title:"在 Linux 服务器使用运维密码",
-        color:"mainRed"
-      },{
-        title:"在 Github 上使用运维密码",
-        color:"mainBlue"
-      },{
-        title:"在 Linux 中国上使用运维密码",
-        color:"mainYellow"
+        title: "在 Github 上使用运维密码",
+        color: "mainBlue",
+        url: "/page/book/github"
+      }, {
+        title: "在 Linux 中国上使用运维密码",
+        color: "mainYellow",
+        url: "/page/book/linuxcn"
       }
     ]
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   }
 })
