cdefc0d27b669275627cdc9b7dad86921caa6649
Bestony <best.tony@foxmail.com>
2017-04-03 21:00:00 +0800

    详情页 完成

--- page/view/view.js
+++ page/view/view.js
@@ -1,22 +1,80 @@
-// page/view/view.js
+const totp = require('../../util/js/totp');
 Page({
-  data:{},
-  onLoad:function(options){
-    wx.setNavigationBarTitle({
-      title: 'Linux中国'
+  data: {},
+  onLoad: function (options) {
+    var that = this;
 
+    /**
+    * 获取数据
+    */
+    var servers = wx.getStorageSync('servers');
+    /**
+     * str to obj
+     */
+    servers = JSON.parse(servers);
+    /**
+     * forEach 处理
+     */
+    servers.forEach(function (value, index, key) {
+      /**
+       * 找到目标数据
+       */
+      if (value.secret == options.id) {
+        that.setData({
+          name: value.name,
+          username: value.username,
+          desc: value.desc,
+          secret: value.secret,
+          code: totp.getCode(options.id),
+          signedBy: value.signedBy,
+          longitude:value.longitude,
+          latitude:value.latitude,
+          "type":value.type
+        })
+        wx.setNavigationBarTitle({
+          title: value.name
+        })
+      }
     })
   },
-  onReady:function(){
-    // 页面渲染完成
+  onReady: function () {
+    var that = this;
+    /**
+     * 进行时间判断，如果时间在30的整除点上，计算一次code，如果不是在时间点上，则只是变更进度条数据
+     */
+    setInterval(function () {
+      var timestamp = new Date().getTime().toString().substr(0, 10);
+      var timeHook = timestamp % 30;
+      if (timeHook != 0) {
+        that.setData({
+          timer: timeHook * 3.4
+        })
+      } else {
+        that.setData({
+          timer: 0
+        })
+        that.updateCode();
+      }
+
+    }, 1000)
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
-  }
+  },
+  updateCode: function () {
+    var that = this;
+    /**
+     * secret存放在页面的data中，直接调用totp.js方法即可生成新的code
+     */
+    var newToken = totp.getCode(this.data.secret);
+    that.setData({
+      code: newToken
+    })
+  },
 })
